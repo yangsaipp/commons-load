3afb13fd0a72650399188485e2f014261b63c33f
yangsai <yangsaipp@qq.com>
2015-11-12 09:39:02 +0800

    类加入comtop版权信息

--- src/main/java/com/comtop/cap/component/loader/LoaderFactory.java
+++ src/main/java/com/comtop/cap/component/loader/LoaderFactory.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (C) 2014 ShenZhen ComTop Information Technology Co.,Ltd
+ * All Rights Reserved.
+ * 本软件为深圳康拓普开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、
+ * 复制、修改或发布本软件.
+ *****************************************************************************/
+
 package com.comtop.cap.component.loader;
 
 import com.comtop.cap.component.loader.config.LoaderConfig;
